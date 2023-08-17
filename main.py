900600711b70d28885a9ed33cc695d062daa05b1
aaam1t <69773892+aaam1t@users.noreply.github.com>
2023-08-17 21:50:14 +1000

    Successfully tested application routes

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+import json
+import motor.motor_asyncio
 import uvicorn
+from bson import ObjectId, json_util
+from dotenv import dotenv_values
+from fastapi import FastAPI, Body, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import Response, JSONResponse
 
+config = dotenv_values(".env")
 app = FastAPI()
+client = motor.motor_asyncio.AsyncIOMotorClient(config["ATLAS_URI"])
+db = client.CourseRecommendationSystem
 
 @app.get("/")
-async def root():
-    return {"message": "successful request"}
+async def get_course_test():
+    courses = await db["Courses"].find().to_list(2)
+    return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(courses)))
