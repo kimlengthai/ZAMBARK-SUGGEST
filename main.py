8f9ba684ef71998a695aa527ce27e33a78cee246
aaam1t <69773892+aaam1t@users.noreply.github.com>
2023-08-21 00:36:48 +1000

    Test path and query paramters

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ client = motor.motor_asyncio.AsyncIOMotorClient(config["ATLAS_URI"])
 db = client.CourseRecommendationSystem
 
 @app.get("/")
-async def get_course_test():
+async def get_course_test(duration: int | None = None):
+    if duration:
+        if (courses := await db["Courses"].find_one({"duration": int(duration)})) is not None:
+            return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(courses)))
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Course with duration {duration} not found")
     courses = await db["Courses"].find().to_list(2)
     return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(courses)))
+
+@app.get("/faculty/{interest}")
+async def get_course_from_interest_path_test(interest: str):
+    if (courses := await db["Courses"].find({"interests": interest}).to_list(2)) is not None:
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(courses)))
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Course with interest {interest} not found")
