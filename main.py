7ce4ce658b1755061c232444f4dc59ebb3f3c6d8
aaam1t <69773892+aaam1t@users.noreply.github.com>
2023-08-27 13:38:05 +1000

    Added filter by major and availability

--- main.py
+++ main.py
@@ -15,8 +15,12 @@ db = client["CourseRecommendationSystem"]
 subjects = db["subjects"]
 
 @app.get("/subjects/")
-async def get_subject(availbility: str | None = None, discipline: str | None = None, interests: Annotated[list[str] | None, Query()] = None):
-    result = await subjects.aggregate([
+async def get_subject(discipline: str | None = None,
+                      availability: Annotated[list[str], Query()] = ["autumn", "spring"],
+                      interests: Annotated[list[str] | None, Query()] = None):
+    if len(result := await subjects.aggregate([
+        {"$match": {"discipline": discipline}},
+        {"$match": {"availability": {"$in": availability}}},
         {"$match": {"interests": {"$in": interests}}},
         {"$project": {
             "code": 1,
@@ -31,5 +35,6 @@ async def get_subject(availbility: str | None = None, discipline: str | None = N
             }
         }},
         {"$sort": {"matches": -1}}
-    ]).to_list(length=None)
-    return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(result)))
+    ]).to_list(length=None)) > 0:
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(result)))
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No matching courses found")
