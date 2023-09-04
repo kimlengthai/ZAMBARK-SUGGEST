1abe07768281e514c1ba0b3246a5e5b0fe512ce7
aaam1t <69773892+aaam1t@users.noreply.github.com>
2023-09-05 00:26:21 +1000

    Update docs

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Body, HTTPException, status, Query
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import Response, JSONResponse
+from pydantic import BaseModel
 from typing import Annotated
 
 load_dotenv()
@@ -16,6 +17,15 @@ app = FastAPI()
 client = motor.motor_asyncio.AsyncIOMotorClient(ATLAS_URI)
 subjects = client["subjects"]
 
+class result(BaseModel):
+    mongo_id: dict
+    code: str
+    name: str
+    credits: int
+    availability: list[str]
+    interests: list[str]
+    matches: int
+
 @app.get("/")
 async def test_atlas_connection():
     try:
@@ -24,10 +34,10 @@ async def test_atlas_connection():
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
-@app.get("/subjects/{faculty}/")
+@app.get("/subjects/{faculty}/", response_model=list[result])
 async def get_subject(faculty: str,
-                      availability: Annotated[list[str], Query()] = ["autumn", "spring"],
-                      interests: Annotated[list[str] | None, Query()] = None):
+                      interests: Annotated[list[str], Query(min_length=3)],
+                      availability: Annotated[list[str], Query()] = ["autumn", "spring", "summer"]):
     if len(result := await subjects[faculty].aggregate([
         {"$match": {"availability": {"$in": availability}}},
         {"$match": {"interests": {"$in": interests}}},
@@ -44,6 +54,6 @@ async def get_subject(faculty: str,
             }
         }},
         {"$sort": {"matches": -1}}
-    ]).to_list(length=None)) > 0:
+    ]).to_list(length=5)) > 0:
         return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(result)))
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No matching courses found")
