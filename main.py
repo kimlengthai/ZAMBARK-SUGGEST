25e7f5cd8311fbf54e7ae0f26ffebbd1be44d92f
aaam1t <69773892+aaam1t@users.noreply.github.com>
2023-09-05 20:10:44 +1000

    Test get current user

--- main.py
+++ main.py
@@ -4,27 +4,41 @@ import os
 import uvicorn
 from bson import ObjectId, json_util
 from dotenv import load_dotenv
-from fastapi import FastAPI, Body, HTTPException, status, Query
+from fastapi import FastAPI, Body, HTTPException, status, Query, Depends
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import Response, JSONResponse
-from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel, Field
 from typing import Annotated
 
 load_dotenv()
 ATLAS_URI = os.getenv("ATLAS_URI")
 
 app = FastAPI()
+oauth2Scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 client = motor.motor_asyncio.AsyncIOMotorClient(ATLAS_URI)
 subjects = client["subjects"]
 
-class result(BaseModel):
-    mongo_id: dict
-    code: str
+class Result(BaseModel):
+    id: str = Field(alias="_id")
     name: str
     credits: int
     availability: list[str]
     interests: list[str]
     matches: int
+
+class User(BaseModel):
+    username: str
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded"
+    )
+
+async def get_current_user(token: Annotated[str, Depends(oauth2Scheme)]):
+    user = fake_decode_token(token)
+    return user
 
 @app.get("/")
 async def test_atlas_connection():
@@ -34,15 +48,15 @@ async def test_atlas_connection():
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
-@app.get("/subjects/{faculty}/", response_model=list[result])
-async def get_subject(faculty: str,
+@app.get("/subjects/{faculty}/", response_model=list[Result])
+async def get_recommendations(faculty: str,
                       interests: Annotated[list[str], Query(min_length=3)],
                       availability: Annotated[list[str], Query()] = ["autumn", "spring", "summer"]):
     if len(result := await subjects[faculty].aggregate([
         {"$match": {"availability": {"$in": availability}}},
         {"$match": {"interests": {"$in": interests}}},
         {"$project": {
-            "code": 1,
+            "_id": 1,
             "name": 1,
             "credits": 1,
             "availability": 1,
@@ -57,3 +71,7 @@ async def get_subject(faculty: str,
     ]).to_list(length=5)) > 0:
         return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(result)))
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No matching courses found")
+
+@app.get("/usertest/")
+async def user_test(token: Annotated[str, Depends(get_current_user)]):
+    return {"token": token}
