9936d8528f5abb98568cdefd947a88bab88fbd11
ahmyk9 <ahmedkhilji99@gmail.com>
2023-10-17 19:19:12 +1100

    users can now scroll through history with buttons

--- profile.js
+++ profile.js
@@ -1,3 +1,5 @@
+let itValue = 0
+
 
 async function profileFetcher (){
     // https://17w1ig90pc.execute-api.ap-southeast-2.amazonaws.com/live/users/amit%40email.com/?it=0
@@ -6,12 +8,11 @@ async function profileFetcher (){
     const profileEmail = profileProperties.replace('@', '%40');
 
     console.log(profileEmail)
-    // const history = await fetch(`https://17w1ig90pc.execute-api.ap-southeast-2.amazonaws.com/live/users/amit%40email.com/?it=0`) 
-    // const history = await fetch(`https://17w1ig90pc.execute-api.ap-southeast-2.amazonaws.com/live/users/amit%40email.com/?it=0`) 
-    const history = await fetch(`https://17w1ig90pc.execute-api.ap-southeast-2.amazonaws.com/live/users/${profileEmail}/?it=0`) 
+
+    const history = await fetch(`https://17w1ig90pc.execute-api.ap-southeast-2.amazonaws.com/live/users/${profileEmail}/?it=${itValue}`) 
     const historyData = await history.json()
     const historyListEl = document.querySelector(".row__subjects");   
-    // const rec = historyData[0].rec[1]
+
     console.log(history)
     historyListEl.innerHTML = historyData[0].rec.map((history) => userHTML(history)).join()
 
@@ -53,74 +54,18 @@ function userHTML(history){
     `
 }
 
-// let currentIndex = 0; // Initialize the current index
+function nextButtonClick() {
+    itValue++; // Increment the "it" value
+    profileFetcher(); // Fetch and update data with the new "it" value
+    console.log(history)
+}
 
-// async function profileFetcher() {
-//   const history = await fetch(
-//     "https://17w1ig90pc.execute-api.ap-southeast-2.amazonaws.com/live/users/amit%40email.com/?it=0"
-//   );
-//   const historyData = await history.json();
+function prevButtonClick() {
+    if (itValue > 0) {
+        itValue--; // Decrement the "it" value (ensure it doesn't go below 0)
+        profileFetcher(); // Fetch and update data with the new "it" value
+        console.log(history)
+    }
+}
 
-//   const historyListEl = document.querySelector(".row__subjects");
 
-//   // Function to update the content based on the current index
-//   function updateContent() {
-//     historyListEl.innerHTML = userHTML(historyData[0].rec[currentIndex]);
-//   }
-
-//   // Initialize content
-//   updateContent();
-
-//   const profileElement = document.querySelector(".profile__body");
-
-//   const userName = profileElement.querySelector("#user__Name");
-//   userName.innerHTML = historyData[0].username;
-
-//   const userEmail = profileElement.querySelector("#user__Email");
-//   userEmail.innerHTML = historyData[0].email;
-
-//   const nextButton = document.getElementById("nextButton");
-//   const prevButton = document.getElementById("prevButton");
-
-//   // Add event listeners for the Next and Previous buttons
-//   nextButton.addEventListener("click", () => {
-//     if (currentIndex < historyData[0].rec.length - 1) {
-//       currentIndex++;
-//       updateContent();
-//     }
-//   });
-
-//   prevButton.addEventListener("click", () => {
-//     if (currentIndex > 0) {
-//       currentIndex--;
-//       updateContent();
-//     }
-//   });
-
-//   return historyData;
-// }
-
-// function userHTML(history) {
-//   return `
-//     <div class="column">
-//       <div class="card">
-//         <h2 id="subjectName">${history.name}</h2>
-
-//         <div class="logoPlace">
-//           <img src="${history.image}" alt="">
-//         </div>
-
-//         <p class="card__title"> Description </p>
-//         <p id="desc">${history.description}</p>
-//         <p class="card__title"> Student Review </p>
-//         <p><br>"<span id="review">${history.review}</span>"<br></p>
-//         <p><br></p>
-//         <p id="difficulty">Difficulty Level: ${history.difficulty}</p>
-//       </div>
-//     </div>
-//   `;
-// }
-
-// profileFetcher();
-
-  
